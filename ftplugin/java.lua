0c1765ead7da26a4fc0bac0afe0bbd77feea6634
Zupanjac <luka.ivkic@gmail.com>
2025-08-11 09:42:56 +0200

    Update java for linux

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,13 +1,15 @@
 -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
 local root_patterns = { 'gradlew', 'build-atlas.xml', '.git', 'setup.py' }
+local os = vim.loop.os_uname().sysname
 local root_dir = vim.fs.dirname(vim.fs.find('atlas', { upward = true })[1])
+
 local config = {
   -- The command that starts the language server
   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = {
 
     -- 💀
-    'C:\\Program Files\\Eclipse Adoptium\\jdk-21.0.5.11-hotspot\\bin\\java', -- or '/path/to/java17_or_newer/bin/java'
+    'java',
     -- depends on if `java` is in your $PATH env variable and if it points to the right version.
 
     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
@@ -24,14 +26,14 @@ local config = {
 
     -- 💀
     '-jar',
-    'C:/tools/jdt-language-server-1.42.0/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar',
+    '/home/epilog/git_source/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar',
     -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
     -- Must point to the                                                     Change this to
     -- eclipse.jdt.ls installation                                           the actual version
 
     -- 💀
     '-configuration',
-    'C:/tools/jdt-language-server-1.42.0/config_win',
+    '/home/epilog/git_source/jdtls/config_linux',
     -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
     -- Must point to the                      Change to one of `linux`, `win` or `mac`
     -- eclipse.jdt.ls installation            Depending on your system.
@@ -48,14 +50,11 @@ local config = {
   --
   -- vim.fs.root requires Neovim 0.10.
   -- If you're using an earlier version, use: require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
-  root_dir = vim.fs.root(0, { '.git', 'settings.gradle', 'build.xml', 'gradlew.bat' }),
 
   -- Here you can configure eclipse.jdt.ls specific settings
   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
   -- for a list of options
-  settings = {
-    java = {},
-  },
+  settings = {},
 
   -- Language server `initializationOptions`
   -- You need to extend the `bundles` with paths to jar files
@@ -87,6 +86,4 @@ local config = {
     bundles = {},
   },
 }
--- This starts a new client & server,
--- or attaches to an existing client & server depending on the `root_dir`.
 require('jdtls').start_or_attach(config)
